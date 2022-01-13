718c05715eae81d28c69142a45d26903799f0adf
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2022-01-13 18:26:54 +0100

    Create function to send email. Function works in console and sends an email when app starts

--- app.js
+++ app.js
@@ -4,6 +4,34 @@ const nodemailer = require('nodemailer');
 const port = 3000;
 const app = express();
 
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: '',
+        pass: '',
+    },
+    tls: {
+        rejectUnauthorized: false,
+    },
+});
+
+let mailOptions = {
+    from: '',
+    to: '',
+    subject: '',
+    text: '',
+};
+
+transporter.sendMail(mailOptions, function(err, success) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Email was sent');
+    };
+});
+
+
+
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
