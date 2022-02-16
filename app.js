1f11647692a8e48473ec4cf30b1cb43ce39a432a
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2022-02-16 20:30:58 +0100

    Create form in html template to post email title and content. Use post method to send mail using button in form

--- app.js
+++ app.js
@@ -1,38 +1,45 @@
 const express = require('express');
+const bp = require('body-parser');
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const port = 3000;
 const app = express();
 
-let transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
-    auth: {
-        user: process.env.SENDER,
-        pass: process.env.PASSWORD,
-    },
-    tls: {
-        rejectUnauthorized: false,
-    },
+app.use(express.static('./public'));
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }))
+
+
+app.post('/', function(req, res) {
+
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.SENDER,
+            pass: process.env.PASSWORD,
+        },
+        tls: {
+            rejectUnauthorized: false,
+        },
+    });
+    
+    let mailDetails = {
+        from: process.env.SENDER,
+        to: process.env.RECIPIENT,
+        subject: req.body.title,
+        text: req.body.message,
+    };
+    
+    transporter
+        .sendMail(mailDetails)
+        .then(() => console.log('Email was sent'))
+        .catch((err) => console.log(err));
+
+    res.sendStatus(200);
 });
-
-let mailDetails = {
-    from: process.env.SENDER,
-    to: process.env.RECIPIENT,
-    subject: '',
-    text: '',
-};
-
-// transporter
-//     .sendMail(mailDetails)
-//     .then(() => console.log('Email was sent'))
-//     .catch((err) => console.log(err));
-
-
-app.use(express.static('./public'))
-
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
