e00c1bb65f0c21a33e4c7936d03036eb6e8a0032
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2022-02-22 15:08:32 +0100

    Move sending function to controllers folder

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bp = require('body-parser');
-const nodemailer = require('nodemailer');
-require('dotenv').config();
+const email = require('./controllers/email')
 
 const port = 3000;
 const app = express();
@@ -10,35 +9,7 @@ app.use(express.static('./public'));
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }))
 
-
-app.post('/', function(req, res) {
-    let transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: false,
-        auth: {
-            user: process.env.SENDER,
-            pass: process.env.PASSWORD,
-        },
-        tls: {
-            rejectUnauthorized: false,
-        },
-    });
-    
-    let mailDetails = {
-        from: process.env.SENDER,
-        to: req.body.recipient,
-        subject: req.body.title,
-        text: req.body.message,
-    };
-    
-    transporter
-        .sendMail(mailDetails)
-        .then(() => console.log(`Email was sent to ${req.body.recipient}`))
-        .catch((err) => console.log(err));
-
-    res.sendStatus(200);
-});
+app.post('/', email);
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
---
+++ controllers/email.js
@@ -0,0 +1,33 @@
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+email = (req, res) => {
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.SENDER,
+            pass: process.env.PASSWORD,
+        },
+        tls: {
+            rejectUnauthorized: false,
+        },
+    });
+    
+    let mailDetails = {
+        from: process.env.SENDER,
+        to: req.body.recipient,
+        subject: req.body.title,
+        text: req.body.message,
+    };
+    
+    transporter
+        .sendMail(mailDetails)
+        .then(() => console.log(`Email was sent to ${req.body.recipient}`))
+        .catch((err) => console.log(err));
+
+    res.sendStatus(200);
+}
+
+module.exports = email
